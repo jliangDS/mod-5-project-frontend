cf9aa7775ea18e9672af84c657c9798168f3709b
Jun Liang <illuminatemynight@gmail.com>
2020-01-03 12:09:38 -0600

    edit checkout amount

--- react-frontend/src/Checkout/CheckoutForm.js
+++ react-frontend/src/Checkout/CheckoutForm.js
@@ -26,7 +26,7 @@ class CheckoutForm extends React.Component {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
-                    amount: this.props.estimatedTotal,
+                    amount: this.props.estimatedTotal.toFixed(2) * 100,
                     payload: payload
                 })
             })
